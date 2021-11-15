f72e0605fa700ac55c5d9f46cea2195ce43d037f
Sorrell Sohota <sjs@MBP14inch2021.hitronhub.home>
2021-11-15 10:20:12 -0800

    created a json fake db, using fetch to load new blog data

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,9 +1,9 @@
-import { useState } from "react"
 
 const BlogList = ({blogs, title}) => {
 
-    // const blogs = props.blogs;
+    // const blogs = props.blogs; 
     // const title = props.title
+    // const handleDelete is a function being passed through a prop
 
     return ( 
         <div className="blog-list">
@@ -12,6 +12,9 @@ const BlogList = ({blogs, title}) => {
             <div className="blog-preview" key={blogs.id}>
                 <h2>{blogs.title}</h2>
                 <p>written by {blogs.author}</p>
+                {/* annonymous function on click event to fire function made in home.js. Passing in blog.id to correctly
+                select the chosen blog to delete */}
+                {/* <button onClick={()=> handleDelete(blogs.id)}>Delete Blog</button>  */}
  
             </div>
         ))}
